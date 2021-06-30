48f066a73ebb5f1e9b395358d0870f01b9ac160f
KerBeGo <kerdaben@gmail.com>
2021-06-30 10:41:49 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html' ,
+  //styleUrls: ['app.component.css'],
+  //template:`
+  // <h1>hola de nuevo</h1> 
+  // esto sirve en casos de que no quieres usar el componente de html (es cuestion de gusto)
+//`
+
+})
+export class AppComponent {
+title = "Bases"
+}
+
+
+
+
